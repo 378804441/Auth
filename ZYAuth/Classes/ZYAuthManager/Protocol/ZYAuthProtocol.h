6441d8d0d11e2b24321d26c46772f3b2d61d2305
丁巍 <dingwei2014@xiaochuankeji.cn>
2019-05-06 19:39:42 +0800

    ADD

---
+++ ZYAuth/Classes/ZYAuthManager/Protocol/ZYAuthProtocol.h
@@ -0,0 +1,16 @@
+//
+//  ZYAuthProtocol.h
+//  FBSnapshotTestCase
+//
+//  Created by 丁巍 on 2019/5/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZYAuthProtocol : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZYAuth/Classes/ZYAuthManager/Protocol/ZYAuthProtocol.m
@@ -0,0 +1,12 @@
+//
+//  ZYAuthProtocol.m
+//  FBSnapshotTestCase
+//
+//  Created by 丁巍 on 2019/5/6.
+//
+
+#import "ZYAuthProtocol.h"
+
+@implementation ZYAuthProtocol
+
+@end
